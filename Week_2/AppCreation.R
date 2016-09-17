c5e056da02b180cc6995bd730d50c2adbd1dc03e
Pradeep Reddy <pradeep5007@gmail.com>
2016-09-17 15:33:56 +0530

    Changing consumer key and access tokens

--- Week_2/AppCreation.R
+++ Week_2/AppCreation.R
@@ -16,10 +16,10 @@ requestURL <- "https://api.twitter.com/oauth/request_token"
 accessURL <- "https://api.twitter.com/oauth/access_token"
 authURL <- "https://api.twitter.com/oauth/authorize"
 
-consumerKey <- "n3feHYGuDPAdN59TPTyskjCWz"
-consumerSecret <- "9qs6F8coRAgswnN5BR3x38NQUbg3YCpaJ8yLanJ3KnDIzLKXF5"
-accessToken <- "1466213366-a3vdDalX69MJFdn12wO0hlxLafCkQ2MXM19sRJN"
-accessSecret <- "fBxhillsv52sUoYPDifnEquoqxIX3H3tI0bK7qeE6KWOU"
+consumerKey <- "q1JnLdXNBkvgBiGbNYY03plil"
+consumerSecret <- "zYRJHaObwBznFsCQwNc0VPwx2dn44TJk2NVJQZ6dN7SDB0Y4VC"
+accessToken <- "108017054-wJVau7GYTu538FE2gGPEffjVOK9m0TaYF3y5j0gt"
+accessSecret <- "6QOGmp8OIPjIBT5OghjGgoIL6MkPQLvHwoTQxZfQEuYJG"
 
 setup_twitter_oauth(consumerKey, 
                     consumerSecret, 
